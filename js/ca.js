8ac5291e0a6df6e4376eab91508396ba5f14d49f
EddyNk <enkerabahizi@gmail.com>
2020-12-27 19:15:12 -0700

    initial data definition + program discription

---
+++ js/ca.js
@@ -0,0 +1,22 @@
+/*program challenge: Create a program that simulates cellular automata*/
+/*
+   [Data Definition]
+   
+   cell: a single member of an object that holds a state (div) 
+   
+   state: alive - 0, dead - 1 (Number)
+
+   rule: describes when a cell should be on or off (Number)
+
+   worldState: represent the rules of the current world (Number)
+
+   world: a grid holding all cells and their state (div)
+
+   generation: represents of the current state of the world and its members
+               ([][])
+
+   buttons: - adjusting rules
+            - setting initial cell states
+            - run program
+            - freeze?
+*/
